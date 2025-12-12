3d4a4152e0768c77a16e357cb58ba6f99c61ebca
Sudheerkumar099 <tsudheerkumar09@gmail.com>
2025-12-12 11:55:00 +0530

    .

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -16,35 +16,36 @@ public class EmpWageBuilder {
          this(company,new Random());
      }
 
-    public int totalWageForCompany(){
-        int totalHours=0;
-        int totalDays=0;
-        int totalWage=0;
-        System.out.println("Computig the total Wages for the Company "+company.getCompanyName());
+    public EmpWageBuilder() {
+    }
 
-        while(totalHours<company.getWorkingHours() && totalDays<company.getWorkingDays()){
+    public int totalWageForCompany() {
+        int totalHours = 0;
+        int totalDays = 0;
+        int totalWage = 0;
+        System.out.println("Computig the total Wages for the Company " + company.getCompanyName());
+
+        while (totalHours < company.getWorkingHours() && totalDays < company.getWorkingDays()) {
             totalDays++;
-            int attendance=random.nextInt(3);
-            int hoursWorked=0;
-            if (attendance==0){
-                hoursWorked=0;
-                System.out.println("Day "+totalDays+" absent");
-            }
-            else if (attendance==1){
-                hoursWorked=8;
-                System.out.println("Day "+totalDays+" Full-Time");
-            }
-            else {
+            int attendance = random.nextInt(3);
+            int hoursWorked = 0;
+            if (attendance == 0) {
+                hoursWorked = 0;
+                System.out.println("Day " + totalDays + " absent");
+            } else if (attendance == 1) {
+                hoursWorked = 8;
+                System.out.println("Day " + totalDays + " Full-Time");
+            } else {
                 hoursWorked = 4;
                 System.out.println("Day " + totalDays + " Part-Time");
 
             }
-            int dailywage=hoursWorked*company.getWagePerHour();
-            totalHours+=hoursWorked;
-            totalWage+=dailywage;
-            System.out.println("Hours Worked: "+hoursWorked + "| Daily wage "+dailywage+" |Total Hours:"+totalHours+" |Total Wage till Now :"+totalWage);
+            int dailywage = hoursWorked * company.getWagePerHour();
+            totalHours += hoursWorked;
+            totalWage += dailywage;
+            System.out.println("Hours Worked: " + hoursWorked + "| Daily wage " + dailywage + " |Total Hours:" + totalHours + " |Total Wage till Now :" + totalWage);
         }
-        System.out.println("Total wage for the Company "+company.getCompanyName() +" is "+totalWage);
+        System.out.println("Total wage for the Company " + company.getCompanyName() + " is " + totalWage);
         company.setTotalWage(totalWage);
         company.addDailyWage(totalWage);
         return totalWage;
